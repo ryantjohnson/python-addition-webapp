010e88171eeff1a073392db17fc30c9289ee20b1
ryantjohnson <rjsaxman@gmail.com>
2016-01-28 11:44:59 -0800

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/<name>")
+def index(name="Ryan"):
+	name = request.args.get('name' , name)
+	return "Hello from {}".format(name)
+
+
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1, num2):
+	return '{} + {} = {}'.format(num1, num2, num1+num2)
+
+if __name__ == "__main__":
+	app.run(debug=True, port=5000)
