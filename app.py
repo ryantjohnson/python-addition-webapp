464ec144cf450892d80f43aa32a629109c22fe29
rjsaxman@gmail.com <rjsaxman@gmail.com>
2016-01-28 12:52:20 -0800

    Created templates for modules

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
+
 
 app = Flask(__name__)
 
@@ -17,34 +18,16 @@ def index(name="Ryan"):
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<float:num2>')
 def add(num1, num2):
-	return """
-
-<!DOCTYPE html>
-<html>
-<head><title>Adding</title></head>
-<body>
-<h1>{} + {} = {}</h1>
-</body>
-</html>
-
-""".format(num1, num2, num1+num2)
+	context = {'num1':num1, 'num2':num2}
+	return render_template("add.html", **context)
 
 @app.route("/multiply/<int:num1>/<int:num2>")
 @app.route("/multiply/<float:num1>/<int:num2>")
 @app.route("/multiply/<int:num1>/<float:num2>")
 @app.route("/multiply/<float:num1>/<float:num2>")
 def multiply(num1, num2):
-	return """
-
-<!DOCTYPE html>
-<html>
-<head><title>Multiplying</title></head>
-<body>
-<h1>{} + {} = {}</h1>
-</body>
-</html>
-
-""".format(num1, num2, num1*num2)
+	context = {'num1':num1, 'num2':num2}
+	return render_template("multiply.html", **context)
 
 if __name__ == "__main__":
 	app.run(debug=True, port=5000)
