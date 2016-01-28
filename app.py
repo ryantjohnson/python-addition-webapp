4e62952a7c58a88a22e25eb544bbea6b89438d6a
rjsaxman@gmail.com <rjsaxman@gmail.com>
2016-01-28 12:18:05 -0800

    Can now add or multiply floats or ints

--- app.py
+++ app.py
@@ -13,10 +13,16 @@ def index(name="Ryan"):
 
 
 @app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+@app.route('/add/<float:num1>/<float:num2>')
 def add(num1, num2):
 	return '{} + {} = {}'.format(num1, num2, num1+num2)
 
-@app.route('/multiply/<int:num1>/<int:num2>')
+@app.route("/multiply/<int:num1>/<int:num2>")
+@app.route("/multiply/<float:num1>/<int:num2>")
+@app.route("/multiply/<int:num1>/<float:num2>")
+@app.route("/multiply/<float:num1>/<float:num2>")
 def multiply(num1, num2):
 	return '{} * {} = {}'.format(num1, num2, num1*num2)
 
