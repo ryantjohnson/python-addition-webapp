fba79067f6814e7a1879f4d43576f9fa15e6dfc9
rjsaxman@gmail.com <rjsaxman@gmail.com>
2016-01-28 12:39:49 -0800

    Added HTML markup

--- app.py
+++ app.py
@@ -17,14 +17,34 @@ def index(name="Ryan"):
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<float:num2>')
 def add(num1, num2):
-	return '{} + {} = {}'.format(num1, num2, num1+num2)
+	return """
+
+<!DOCTYPE html>
+<html>
+<head><title>Adding</title></head>
+<body>
+<h1>{} + {} = {}</h1>
+</body>
+</html>
+
+""".format(num1, num2, num1+num2)
 
 @app.route("/multiply/<int:num1>/<int:num2>")
 @app.route("/multiply/<float:num1>/<int:num2>")
 @app.route("/multiply/<int:num1>/<float:num2>")
 @app.route("/multiply/<float:num1>/<float:num2>")
 def multiply(num1, num2):
-	return '{} * {} = {}'.format(num1, num2, num1*num2)
+	return """
+
+<!DOCTYPE html>
+<html>
+<head><title>Multiplying</title></head>
+<body>
+<h1>{} + {} = {}</h1>
+</body>
+</html>
+
+""".format(num1, num2, num1*num2)
 
 if __name__ == "__main__":
 	app.run(debug=True, port=5000)
