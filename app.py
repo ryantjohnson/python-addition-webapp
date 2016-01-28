f82422aa1db4ce4fc5faf6f27931125d6aa9c33f
rjsaxman@gmail.com <rjsaxman@gmail.com>
2016-01-28 12:08:19 -0800

    Added multiply module

--- app.py
+++ app.py
@@ -9,12 +9,16 @@ app = Flask(__name__)
 @app.route("/<name>")
 def index(name="Ryan"):
 	name = request.args.get('name' , name)
-	return "Hello from {}".format(name)
+	return "Hello, {}!".format(name)
 
 
 @app.route('/add/<int:num1>/<int:num2>')
 def add(num1, num2):
 	return '{} + {} = {}'.format(num1, num2, num1+num2)
+
+@app.route('/multiply/<int:num1>/<int:num2>')
+def multiply(num1, num2):
+	return '{} * {} = {}'.format(num1, num2, num1*num2)
 
 if __name__ == "__main__":
 	app.run(debug=True, port=5000)
